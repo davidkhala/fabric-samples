a5ae3eafa3c5bfc8c30ba7e8041d9f5fb968476b
Yuxiang LIU <david-khala@hotmail.com>
2017-07-04 09:48:42 +0800

    Fix commit

--- balance-transfer/runApp.sh
+++ balance-transfer/runApp.sh
@@ -5,44 +5,54 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function dkcl(){
-        CONTAINER_IDS=$(docker ps -aq)
+UP_DOWN="$1"
+FILTER="$2"
+
+function dkcl() {
+	if [ -z "$FILTER" ]; then
+		FILTER='peer|example.com'
+	fi
+	CONTAINER_IDS=$(docker ps -aq -f name="$FILTER")
+
 	echo
-        if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
-                echo "========== No containers available for deletion =========="
-        else
-                docker rm -f $CONTAINER_IDS
-        fi
+	if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
+		echo "========== No containers available for deletion =========="
+	else
+		docker rm -f $CONTAINER_IDS
+	fi
 	echo
 }
 
-function dkrm(){
-        DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+function dkrm() {
+	DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
 	echo
-        if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+	if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
 		echo "========== No images available for deletion ==========="
-        else
-                docker rmi -f $DOCKER_IMAGE_IDS
-        fi
+	else
+		docker rmi -f $DOCKER_IMAGE_IDS
+	fi
 	echo
 }
 
-function restartNetwork() {
-	echo
-
-        #teardown the network and clean the containers and intermediate images
+function networkDown() {
+	#teardown the network and clean the containers and intermediate images
 	cd artifacts
 	docker-compose down
 	dkcl
 	dkrm
-
 	#Cleanup the material
 	rm -rf /tmp/hfc-test-kvs_peerOrg* $HOME/.hfc-key-store/ /tmp/fabric-client-kvs_peerOrg*
+	cd -
+}
 
+function networkUp() {
+	cd artifacts
 	#Start the network
 	docker-compose up -d
 	cd -
-	echo
+	installNodeModules
+
+	PORT=4000 node app
 }
 
 function installNodeModules() {
@@ -56,9 +66,18 @@ function installNodeModules() {
 	echo
 }
 
+if [ "${UP_DOWN}" == "up" ]; then
+	networkUp
+elif [ "${UP_DOWN}" == "down" ]; then ## Clear the network
+	networkDown
+elif [ "${UP_DOWN}" == "restart" ]; then ## Restart the network
+	networkDown
+	networkUp
+elif [ "${UP_DOWN}" == "resume" ]; then ## Resume the network
+	networkResume
+elif [ "${UP_DOWN}" == "pause" ]; then ## Pause the network
+	networkPause
+else
 
-restartNetwork
-
-installNodeModules
-
-PORT=4000 node app
+	exit 1
+fi
--- balance-transfer/testAPIs.sh
+++ balance-transfer/testAPIs.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-jq --version > /dev/null 2>&1
+jq --version >/dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "Please Install 'jq' https://stedolan.github.io/jq/ to execute this script"
 	echo
@@ -16,9 +16,9 @@ starttime=$(date +%s)
 echo "POST request Enroll on Org1  ..."
 echo
 ORG1_TOKEN=$(curl -s -X POST \
-  http://localhost:4000/users \
-  -H "content-type: application/x-www-form-urlencoded" \
-  -d 'username=Jim&orgName=org1')
+	http://localhost:4000/users \
+	-H "content-type: application/x-www-form-urlencoded" \
+	-d 'username=Jim&orgName=org1')
 echo $ORG1_TOKEN
 ORG1_TOKEN=$(echo $ORG1_TOKEN | jq ".token" | sed "s/\"//g")
 echo
@@ -27,9 +27,9 @@ echo
 echo "POST request Enroll on Org2 ..."
 echo
 ORG2_TOKEN=$(curl -s -X POST \
-  http://localhost:4000/users \
-  -H "content-type: application/x-www-form-urlencoded" \
-  -d 'username=Barry&orgName=org2')
+	http://localhost:4000/users \
+	-H "content-type: application/x-www-form-urlencoded" \
+	-d 'username=Barry&orgName=org2')
 echo $ORG2_TOKEN
 ORG2_TOKEN=$(echo $ORG2_TOKEN | jq ".token" | sed "s/\"//g")
 echo
@@ -39,10 +39,10 @@ echo
 echo "POST request Create channel  ..."
 echo
 curl -s -X POST \
-  http://localhost:4000/channels \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/channels \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"channelName":"mychannel",
 	"channelConfigPath":"../artifacts/channel/mychannel.tx"
 }'
@@ -52,10 +52,10 @@ sleep 5
 echo "POST request Join channel on Org1"
 echo
 curl -s -X POST \
-  http://localhost:4000/channels/mychannel/peers \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/channels/mychannel/peers \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"peers": ["localhost:7051","localhost:7056"]
 }'
 echo
@@ -64,10 +64,10 @@ echo
 echo "POST request Join channel on Org2"
 echo
 curl -s -X POST \
-  http://localhost:4000/channels/mychannel/peers \
-  -H "authorization: Bearer $ORG2_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/channels/mychannel/peers \
+	-H "authorization: Bearer $ORG2_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"peers": ["localhost:8051","localhost:8056"]
 }'
 echo
@@ -76,10 +76,10 @@ echo
 echo "POST Install chaincode on Org1"
 echo
 curl -s -X POST \
-  http://localhost:4000/chaincodes \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/chaincodes \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"peers": ["localhost:7051","localhost:7056"],
 	"chaincodeName":"mycc",
 	"chaincodePath":"github.com/example_cc",
@@ -88,14 +88,13 @@ curl -s -X POST \
 echo
 echo
 
-
 echo "POST Install chaincode on Org2"
 echo
 curl -s -X POST \
-  http://localhost:4000/chaincodes \
-  -H "authorization: Bearer $ORG2_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/chaincodes \
+	-H "authorization: Bearer $ORG2_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"peers": ["localhost:8051","localhost:8056"],
 	"chaincodeName":"mycc",
 	"chaincodePath":"github.com/example_cc",
@@ -107,10 +106,10 @@ echo
 echo "POST instantiate chaincode on peer1 of Org1"
 echo
 curl -s -X POST \
-  http://localhost:4000/channels/mychannel/chaincodes \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/channels/mychannel/chaincodes \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"chaincodeName":"mycc",
 	"chaincodeVersion":"v0",
 	"functionName":"init",
@@ -122,10 +121,10 @@ echo
 echo "POST invoke chaincode on peers of Org1 and Org2"
 echo
 TRX_ID=$(curl -s -X POST \
-  http://localhost:4000/channels/mychannel/chaincodes/mycc \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json" \
-  -d '{
+	http://localhost:4000/channels/mychannel/chaincodes/mycc \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json" \
+	-d '{
 	"peers": ["localhost:7051", "localhost:8051"],
 	"fcn":"move",
 	"args":["a","b","10"]
@@ -137,26 +136,26 @@ echo
 echo "GET query chaincode on peer1 of Org1"
 echo
 curl -s -X GET \
-  "http://localhost:4000/channels/mychannel/chaincodes/mycc?peer=peer1&fcn=query&args=%5B%22a%22%5D" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	"http://localhost:4000/channels/mychannel/chaincodes/mycc?peer=peer1&fcn=query&args=%5B%22a%22%5D" \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
 echo "GET query Block by blockNumber"
 echo
 curl -s -X GET \
-  "http://localhost:4000/channels/mychannel/blocks/1?peer=peer1" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	"http://localhost:4000/channels/mychannel/blocks/1?peer=peer1" \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
 echo "GET query Transaction by TransactionID"
 echo
 curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID?peer=peer1 \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
@@ -178,38 +177,37 @@ echo
 echo "GET query ChainInfo"
 echo
 curl -s -X GET \
-  "http://localhost:4000/channels/mychannel?peer=peer1" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	"http://localhost:4000/channels/mychannel?peer=peer1" \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
 echo "GET query Installed chaincodes"
 echo
 curl -s -X GET \
-  "http://localhost:4000/chaincodes?peer=peer1&type=installed" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	"http://localhost:4000/chaincodes?peer=peer1&type=installed" \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
 echo "GET query Instantiated chaincodes"
 echo
 curl -s -X GET \
-  "http://localhost:4000/chaincodes?peer=peer1&type=instantiated" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	"http://localhost:4000/chaincodes?peer=peer1&type=instantiated" \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
 echo "GET query Channels"
 echo
 curl -s -X GET \
-  "http://localhost:4000/channels?peer=peer1" \
-  -H "authorization: Bearer $ORG1_TOKEN" \
-  -H "content-type: application/json"
+	"http://localhost:4000/channels?peer=peer1" \
+	-H "authorization: Bearer $ORG1_TOKEN" \
+	-H "content-type: application/json"
 echo
 echo
 
-
-echo "Total execution time : $(($(date +%s)-starttime)) secs ..."
+echo "Total execution time : $(($(date +%s) - starttime)) secs ..."
