533362424d0da5bb3b2f9038763a6d3385016df6
Yuxiang LIU <david-khala@hotmail.com>
2017-07-17 15:53:19 +0800

    inspect cryptogen

---
+++ common/cryptogen/runCryptogen.sh
@@ -0,0 +1,22 @@
+CURRENT=$PWD
+CONFIG_OUTPUT=$CURRENT/crypto-config
+CONFIG_INPUT=$CURRENT/cryptogen.yaml
+# clear existing
+MODE=$1
+
+function clearOutput() {
+	echo "clear CONFIG_OUTPUT $CONFIG_OUTPUT"
+	rm -rf $CONFIG_OUTPUT
+}
+
+function gen() {
+	cd ../../bin
+
+	./cryptogen generate --config="$CONFIG_INPUT" --output="$CONFIG_OUTPUT"
+
+	cd -
+}
+if [ "${MODE}" == "clear" ]; then
+	clearOutput
+fi
+gen
